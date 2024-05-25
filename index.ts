e6f3e543a92d62de942e3c10cc95d2368156cf0d
mominasudozai <168901283+mominasudozai@users.noreply.github.com>
2024-05-24 22:11:24 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,32 @@
+import inquirer from "inquirer";
+
+// Asking questions from through Inquirer
+
+let answers = await inquirer.prompt([
+    {message: "Enter First Number", type: "number", name: "firstNumber"},
+    {message: "Enter Second Number", type:"number", name: "secondNumber"},
+    {
+        message: "select one Operator to perform Operation",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "division"],
+    },
+]);
+
+// Conditional statements If-Else
+
+if(answers.operator === "Addition"){
+    console.log(answers.firstNumber + answers.secondNumber)
+}
+else if (answers.operator === "Subtractions"){
+    console.log(answers.firstNumber - answers.secondNumber)
+}
+else if (answers.operator === "Multiplication"){
+    console.log(answers.firstNumber * answers.secondNumber)
+}
+else if (answers.operator === "Division"){
+    console.log(answers.firstNumber / answers.secondNumber)
+}
+else{
+    console.log("Invalid Input");
+}
